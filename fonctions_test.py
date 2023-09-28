4189368e96e40e18940fdbcfe87f24153db86180
akira <akira.thirion@gmail.com>
2023-09-28 18:53:53 +0200

    fonction test box_isempty et box_empty

--- fonctions_test.py
+++ fonctions_test.py
@@ -14,6 +14,34 @@ def test_create_matrix():
         for element in row:
             assert element == struct_case
 
+def test_box_isempty():
+    struct_case1 = {"nblock": 0, "Note_list": create_noteslist(), "val": 0} #struture pour matrice1
+    struct_case2 = {"nblock": 0, "Note_list": create_noteslist(), "val": 1} #struture pour matrice2
+    taille = 3  # exemple sur une matrice 3x3
+    matrix1 = create_matrix(taille,struct_case1)          #création de la matrice1
+    matrix2 = create_matrix(taille, struct_case2)         #création de la matrice2
+    assert box_isempty(matrix1,1,1) == True         #test sur une case vide
+    assert box_isempty(matrix2,2,2) == False        #test sur une case non-vide
+
+def test_box_empty():
+    struct_case1 = {"nblock": 0, "Note_list": create_noteslist(), "val": 0} #struture pour matrice1
+    struct_case2 = {"nblock": 0, "Note_list": create_noteslist(), "val": 1} #struture pour matrice2
+    taille = 3 # test sur matrice 3x3
+    matrix1 = create_matrix(taille, struct_case1)  # matrice final voulu
+    matrix2 = create_matrix(taille, struct_case2)  # matrice de test
+    for i in range(taille):
+        for j in range(taille):
+            matrix2=box_empty(matrix2, i, j)
+            matrix2=box_empty(matrix2, i, j)
+    assert matrix2 == matrix1  # compare les deux matrices
+
+
+
+
 if __name__ == '__main__':
     test_create_matrix()
     print("La fonction create_matrix a passé les tests avec succès.")
+    test_box_empty()
+    print("La fonction test_box_empty a passé les tests avec succès.")
+    test_box_isempty()
+    print("La fonction test_box_isempty a passé les tests avec succès.")
